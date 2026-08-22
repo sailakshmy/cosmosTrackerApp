21faf2b9f4c8405912545a64b9e3a98baff86ae0
Sai Lakshmy <sailakshmy94@gmail.com>
2026-08-22 21:52:31 +0400

    Update nasaImageGallery.tsx

--- cosmos-tracker-app/src/app/imageGallery/nasaImageGallery.tsx
+++ cosmos-tracker-app/src/app/imageGallery/nasaImageGallery.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+//import { useCallback } from "react";
 import PolaroidImageCard from "@/components/polaroid-image-card";
 import { SpaceBackground } from "@/components/space-background";
 import { ThemedText } from "@/components/themed-text";
